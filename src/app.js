5841ab6cb31c7fd2a21d62692dbc90106fc2b293
Maximo <maximo1611@gmail.com>
2021-11-12 07:45:55 -0300

    nuevo commit

--- src/app.js
+++ src/app.js
@@ -34,7 +34,7 @@ const UserRoutes = require("./routes/users");
 app.use("/users", UserRoutes);
 
 
-const PropousalsRoustes = require("./routes/propousals");
+const PropousalsRoustes = require("./routes/Propousals");
 app.use("/propousals", PropousalsRoustes);
 
 const Comments = require("./routes/comments");

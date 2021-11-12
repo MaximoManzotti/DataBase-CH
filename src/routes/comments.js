4258a317572f8d2f5b2aaf098f3134f95fdff04c
Maximo <maximo1611@gmail.com>
2021-11-12 08:01:21 -0300

    prueba

--- src/routes/comments.js
+++ src/routes/comments.js
@@ -37,7 +37,7 @@ router.post("/add", checkAuth, (req, res) => {
 res.status(200).json({message: newComment})
 } catch (err) {
         console.log(err);
-        res.status(500).json({ message: "Error while creating propousal;" });
+        res.status(500).json({ message: "Error while creating comment;" });
       }
     }
 })
